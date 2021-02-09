91a4dc70d6e7511e20372a80249b00ab74a253b3
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-09 14:59:20 +0100

    instead of www use server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+
+const port = process.env.PORT || "5000";
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log("Listening to " + port);
+});
