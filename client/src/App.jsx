7aaee162e3da6e37e4933b79bff03215a8284c17
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-11 16:51:21 +0100

    small tweeks so the app is working (import problems)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import GlobalStyle from "./styledComponents/GlobalStyle";
-
+import { useState } from "react";
 import Warning from "./components/Warning";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
-import { bakeyContext } from "../Context";
+import { bakeyContext } from "./Context";
 
 function App() {
-  const [isLogged, setIsLogged] = userState(false);
-  const [userName, setUserName] = userState("");
+  const [isLogged, setIsLogged] = useState(false);
+  const [userName, setUserName] = useState("");
   const [profilePic, setProfilePic] = useState();
 
   return (
-    <bakeyContext.Provider value={{isLogged, setIsLogged, userName, setUserName, profilePic, setProfilePic}}>
+    <bakeyContext.Provider
+      value={{
+        isLogged,
+        setIsLogged,
+        userName,
+        setUserName,
+        profilePic,
+        setProfilePic,
+      }}
+    >
       <GlobalStyle />
       <Warning msg="site is not found" />
       <Registration />
