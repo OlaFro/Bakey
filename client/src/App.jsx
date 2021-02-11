472084b1ba9bffeaa1dd497bc3dc489754afc17a
WILLOID <socialguille@gmail.com>
2021-02-11 16:01:22 +0100

    added context.provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,20 @@ import GlobalStyle from "./styledComponents/GlobalStyle";
 import Warning from "./components/Warning";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
-import {bakeyContext} from "../Context"
-
+import { bakeyContext } from "../Context";
 
 function App() {
-
   const [isLogged, setIsLogged] = userState(false);
-  const [userName, setUserName] = userState('');
+  const [userName, setUserName] = userState("");
   const [profilePic, setProfilePic] = useState();
 
   return (
-    <div>
+    <bakeyContext.Provider value={{isLogged, setIsLogged, userName, setUserName, profilePic, setProfilePic}}>
       <GlobalStyle />
       <Warning msg="site is not found" />
       <Registration />
       <Login />
-    </div>
+    </bakeyContext.Provider>
   );
 }
 
