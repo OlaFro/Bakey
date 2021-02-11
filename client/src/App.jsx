ad5933dc771c04ff74d28ee8c0374461a2d4e640
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-11 10:00:20 +0100

    displaying login in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Login from "./components/Login";
 import Registration from "./components/Registration";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <div>
       bakey
       <Registration />
+      <Login />
     </div>
   );
 }
