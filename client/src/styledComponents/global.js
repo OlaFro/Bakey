9b606a64befa3de606325a2d192153a04d64745f
OlaFro <olka.be@gmail.com>
2021-02-11 13:57:02 +0100

    adding values to global

---
+++ client/src/styledComponents/global.js
@@ -0,0 +1,28 @@
+const { createGlobalStyle } = require("styled-components");
+
+const GlobalStyle = createGlobalStyle`
+
+:root {
+  --space-small: 0.75em;
+  --space-medium: 1.5em;
+  --space-larger: 2em;
+  --space-large: 3em;
+}
+
+html {
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: var(--text);
+}
+`;
+
+export default GlobalStyle;
