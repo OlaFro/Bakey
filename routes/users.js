5c93b1a36889583e8eb0baf7c51f826f4f802faa
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-11 16:10:01 +0100

    created endpoints for users, cleaning

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/register", (req, res, next) => {
+  res.send("registration");
+});
+
+router.post("/login", (req, res, next) => {
+  res.send("login");
+});
+
+router.get("/logout", (req, res, next) => {
+  res.send("logout");
+});
+
+router.put("/update", (req, res, next) => {
+  res.send("settings");
 });
 
 module.exports = router;
