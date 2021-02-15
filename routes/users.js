01efb8605e05d4a7c24249e35c862216466c2be5
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-15 10:23:26 +0100

    created functionality for adding user to db

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require("express");
+const validateData = require("../controllers/validControllers");
+const UserModel = require("../models/UserModel");
 var router = express.Router();
 
 /* GET users listing. */
@@ -6,8 +8,43 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.post("/register", (req, res, next) => {
-  res.send("registration");
+router.post("/register", validateData.register, (req, res, next) => {
+  console.log(req.body);
+  let newUser = req.body;
+
+  UserModel.estimatedDocumentCount({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      newUser.id = result + 1;
+      console.log(newUser);
+
+      let addedUser = new UserModel({
+        id: newUser.id,
+        email: newUser.email,
+        password: newUser.password,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        city: newUser.city,
+      });
+
+      bcrypt.hash(newUser.password, 10, (err, hashedPassword) => {
+        if (!err) {
+          addedUser.password = hashedPassword;
+          addedUser
+            .save()
+            .then(() => {
+              res.send({ registered: true });
+            })
+            .catch((err) => {
+              res.send(err);
+            });
+        } else {
+          res.send({ errorSource: "BCRYPT" });
+        }
+      });
+    }
+  });
 });
 
 router.post("/login", (req, res, next) => {
