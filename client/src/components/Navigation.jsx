4cbd3ff3468f2d15bed6712575ccb1de18318f75
WILLOID <socialguille@gmail.com>
2021-02-16 14:02:38 +0100

    created logout button

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext} from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
@@ -8,10 +8,12 @@ import {
   StyledNavContainer,
   StyledNavBtnsContainer,
   StyledLogoContainer,
-  StyledNavBtn
+  StyledNavBtn,
 } from "../styledComponents/StyledNavigation";
 import StyledLogo from "../styledComponents/StyledLogo";
-import {StyledSmallButton} from "../styledComponents/StyledButton"
+import { StyledSmallButton } from "../styledComponents/StyledButton";
+import { bakeyContext } from "../Context";
+import axios from "axios";
 
 export default function Navigation(props) {
   const [open, setOpen] = useState(0);
@@ -23,30 +25,47 @@ export default function Navigation(props) {
   const handleClose = () => {
     setOpen(0);
   };
+
+  const context = useContext(bakeyContext);
+
+  const logout = () => {
+    axios({
+      method:"GET",
+      url: "/users/logout"
+    }).then((res)=>{
+      context.setIsLogged(res.data.logged);
+    }).catch((err) => console.log(err))
+  }
+
   return (
-    <StyledNavigation>
-      <StyledLogoContainer>
-        <StyledLogo>
-          <Link to="/">bakey</Link>
-        </StyledLogo>
-        <StyledExit onClick={handleClose} display={open} />
-        <StyledMenu onClick={handleOpen} display={open} />
-      </StyledLogoContainer>
-      <StyledNavContainer display={open}>
-        <Link to="/">cafés</Link>
-        <Link to="/">about us</Link>
-      </StyledNavContainer>
-      <StyledNavBtnsContainer display={open}>
-        <StyledNavBtn login>
-          <Link to="/login">log in</Link>{" "}
-        </StyledNavBtn>
-        <StyledNavBtn registration>
-          <Link to="/registration/user">register</Link>
-        </StyledNavBtn>
-        <StyledSmallButton>
-          <Link to="/login">Log out</Link>
-        </StyledSmallButton>
-      </StyledNavBtnsContainer>
-    </StyledNavigation>
+   <StyledNavigation>
+        <StyledLogoContainer>
+          <StyledLogo>
+            <Link to="/">bakey</Link>
+          </StyledLogo>
+          <StyledExit onClick={handleClose} display={open} />
+          <StyledMenu onClick={handleOpen} display={open} />
+        </StyledLogoContainer>
+        <StyledNavContainer display={open}>
+          <Link to="/">cafés</Link>
+          <Link to="/">about us</Link>
+        </StyledNavContainer>
+        <StyledNavBtnsContainer display={open}>
+          {context.isLogged ? (
+            <StyledSmallButton onClick={logout}>
+              <Link to="/login">Log out</Link>
+            </StyledSmallButton>
+          ) : (
+            <StyledNavBtn login>
+              <Link to="/login">log in</Link>
+            </StyledNavBtn>
+          )}
+          {context.isLogged ? null : (
+            <StyledNavBtn registration>
+              <Link to="/registration/user">register</Link>
+            </StyledNavBtn>
+          )}
+        </StyledNavBtnsContainer>
+      </StyledNavigation>
   );
 }
