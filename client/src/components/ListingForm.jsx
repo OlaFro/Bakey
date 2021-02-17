e394e28e368050f5c003f27f9bb949d91b111b0f
OlaFro <olka.be@gmail.com>
2021-02-17 14:12:24 +0100

    checkbox for allergenes

--- client/src/components/ListingForm.jsx
+++ client/src/components/ListingForm.jsx
@@ -46,19 +46,24 @@ export default function ListingForm() {
               {/* {msg.firstName ? <small>Please use only letters</small> : null} */}
             </div>
           </StyledInputContainer>
+
           <div>
-            <input type="checkbox" id="vegan" name="vegan" />
-            <label htmlFor="vegan">vegan</label>
-            <input type="checkbox" id="organic" name="organic" />
-            <label htmlFor="organic">organic</label>
-            <input type="checkbox" id="glutenFree" name="glutenFree" />
-            <label htmlFor="glutenFree">gluten-free</label>
-            <input type="checkbox" id="lactoseFree" name="lactoseFree" />
-            <label htmlFor="lactoseFree">lactose-free</label>
-            <input type="checkbox" id="sugarFree" name="sugarFree" />
-            <label htmlFor="sugarFree">sugar-free</label>
-            <input type="checkbox" id="wheatFree" name="wheatFree" />
-            <label htmlFor="wheatFree">wheat-free</label>
+            <input type="checkbox" id="cereal" name="cereal" />
+            <label htmlFor="cereal">cereal</label>
+            <input type="checkbox" id="eggs" name="eggs" />
+            <label htmlFor="eggs">eggs</label>
+            <input type="checkbox" id="peanut" name="peanut" />
+            <label htmlFor="peanut">peanut</label>
+            <input type="checkbox" id="soja" name="soja" />
+            <label htmlFor="soja">soja</label>
+            <input type="checkbox" id="dairy" name="dairy" />
+            <label htmlFor="dairy">dairy</label>
+            <input type="checkbox" id="celery" name="celery" />
+            <label htmlFor="celery">celery</label>
+            <input type="checkbox" id="mustard" name="mustard" />
+            <label htmlFor="mustard">mustard</label>
+            <input type="checkbox" id="lupins" name="lupins" />
+            <label htmlFor="lupins">lupins</label>
           </div>
           <div>
             <input type="checkbox" id="vegan" name="vegan" />
