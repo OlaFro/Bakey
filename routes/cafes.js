1359b95071bf73746c615bad53ae2fb648421b5a
WILLOID <socialguille@gmail.com>
2021-02-18 14:42:14 +0100

    added comment about not need to check connection

--- routes/cafes.js
+++ routes/cafes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const User = require("../models/UserModel");
 
 router.get("/", (req, res, next) => {
+    //no need to check connection bc we connect in the app.js
     const city = req.city;
     User.find({city: city, userType: "cafe"}).then(result => {
         res.send(result)
