8511c514c594b7cb288545817aebc933d06554e1
OlaFro <olka.be@gmail.com>
2021-02-18 14:07:24 +0100

    listing preview deleted from app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,6 @@ import RegistrationCafe from "./components/RegistrationCafe";
 import DashboardUser from "./components/DashboardUser";
 import DashboardCafe from "./components/DashboardCafe";
 import Navigation from "./components/Navigation";
-import ListingForm from "./components/ListingForm";
 
 function App() {
   const [isLogged, setIsLogged] = useState({ state: false, role: "" });
@@ -60,9 +59,7 @@ function App() {
         <Navigation />
         <Switch>
           <Route path="/" exact>
-            {/* <h1>Welcome to bakey</h1>
-            listingForm for preview, will be deleted later */}
-            <ListingForm />
+            <h1>Welcome to bakey</h1>
           </Route>
           <Route path="/registration/user" exact>
             <RegistrationUser />
