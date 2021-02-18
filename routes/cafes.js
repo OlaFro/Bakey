99cdfd17366e965fdfcdcca64aad7cc81cf8c728
WILLOID <socialguille@gmail.com>
2021-02-18 14:16:40 +0100

    1st merge 17.02

--- routes/cafes.js
+++ routes/cafes.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+router.get("/", (req, res, next) => {
+    const city = req.city;
+    let cities
+});
 
 module.exports = router;
