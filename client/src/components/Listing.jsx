6612d2bc16a2d96d5bc6c5bfba89fa8909bd40cf
OlaFro <olka.be@gmail.com>
2021-02-19 10:21:12 +0100

    Basic structure of listing

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -5,13 +5,33 @@ import {
   StyledPhotoContainer,
   StyledDescContainer,
 } from "../styledComponents/StyledListing";
+import { StyledButton } from "../styledComponents/StyledButton";
 
 export default function Listing() {
   const { cafeName } = useContext(bakeyContext);
   return (
     <StyledListingContainer>
       <StyledPhotoContainer></StyledPhotoContainer>
-      <StyledDescContainer></StyledDescContainer>
+      <StyledDescContainer>
+        <h2>Kati's pie</h2>
+        <span>{cafeName}</span>
+        <div>
+          <span>vegan</span>
+          <span>organic</span>
+        </div>
+        <div>
+          <i>Allergenes: eggs, dairy, cereal</i>
+        </div>
+        <div>
+          <i>Possible pick-up: 23/03/21 12:00</i>
+        </div>
+        <div>Counter</div>
+        <div>Time Left</div>
+        <div>
+          <StyledButton>Buy a piece for 10€</StyledButton>
+          <StyledButton>Buy whole for 40€</StyledButton>
+        </div>
+      </StyledDescContainer>
     </StyledListingContainer>
   );
 }
--- client/src/styledComponents/StyledListing.jsx
+++ client/src/styledComponents/StyledListing.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import colors from "../styledComponents/colors";
+import { lighten } from "polished";
 
 export const StyledListingContainer = styled.div`
   width: 90%;
@@ -8,15 +9,18 @@ export const StyledListingContainer = styled.div`
 
   display: grid;
   grid-template-columns: 40% 60%;
+
+  /* just temporal */
+  margin: 10rem 0 25rem 0;
 `;
 
 export const StyledPhotoContainer = styled.div`
   height: 100%;
-  background-image: url("https://images.unsplash.com/photo-1572383672419-ab35444a6934?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80");
+  background-image: url("https://images.unsplash.com/photo-1604184811623-129a299b1322?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1080&q=80");
   background-size: contain;
 `;
 
 export const StyledDescContainer = styled.div`
   height: 100%;
-  background-color: ${colors.gray};
+  background-color: ${lighten(0.25, colors.gray)};
 `;
