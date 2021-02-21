988fce444a1a905414caf7f197d1dea790f86e33
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-21 22:06:38 +0100

    checked cafe route and debugged

--- routes/cafes.js
+++ routes/cafes.js
@@ -2,12 +2,17 @@ var express = require("express");
 var router = express.Router();
 const User = require("../models/UserModel");
 
-router.get("/", (req, res, next) => {
-    //no need to check connection bc we connect in the app.js
-    const city = req.city;
-    User.find({city: city, userType: "cafe"}).select(["-password", "-email"]).then(result => {
-        res.send(result)
-    }).catch((err)=>{res.send(err)})
+router.post("/", (req, res, next) => {
+  //no need to check connection bc we connect in the app.js
+  const city = req.body.city;
+  User.find({ city: city, userType: "cafe" })
+    .select(["-password", "-email"])
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
