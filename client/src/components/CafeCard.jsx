c6b7ca0247f72e903f5b6846dd313ef59ce52d26
OlaFro <olka.be@gmail.com>
2021-02-23 14:52:09 +0100

    implementing progress bar

--- client/src/components/CafeCard.jsx
+++ client/src/components/CafeCard.jsx
@@ -1,32 +1,58 @@
 import React, { useState } from "react";
 import { StyledCafeCard } from "../styledComponents/StyledCafeCard";
-import {
-  StyledTag,
-  StyledTagContainer,
-} from "../styledComponents/StyledListing";
+import { StyledTag } from "../styledComponents/StyledListing";
+
+import colors from "../styledComponents/colors";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export default function ListViewCafe(props) {
   return (
     <StyledCafeCard>
       <header>
         <figure>
-          <img></img>
+          <img
+            src="https://i.pinimg.com/originals/1f/c3/ff/1fc3ff4791f292d4ec65893a2087825c.png"
+            alt="our logo"
+          ></img>
         </figure>
         <div>
-          <h2>Cafe Name</h2>
-          <p>address address</p>
+          <h3>Cafe Ocka</h3>
+          <span>Merseburgerstr. 88, 04177 Leipzig</span>
         </div>
       </header>
       <main>
-        <h3>Running Campaignes</h3>
-        <div>
-          <span>Cheesecake</span>
-          <div>
-            <StyledTag organic title="organic">
-              O
-            </StyledTag>
+        <h3>Running campaignes:</h3>
+        <div className="listing">
+          <div className="cake-info">
+            <span>Cheesecake</span>
+            <div className="tag-container">
+              <StyledTag no lactose title="lactose free">
+                L
+              </StyledTag>
+              <StyledTag no gluten title="gluten free">
+                G
+              </StyledTag>
+              <StyledTag no sugar title="sugar free">
+                S
+              </StyledTag>
+              <StyledTag vegan title="vegan">
+                V
+              </StyledTag>
+              <StyledTag organic title="organic">
+                O
+              </StyledTag>
+              <StyledTag no lactose title="lactose free">
+                L
+              </StyledTag>
+            </div>
           </div>
-          <div>progress bar</div>
+          <div className="progressBar">
+            <ProgressBar
+              completed={60}
+              bgcolor={colors.accent1}
+              isLabelVisible={false}
+            />
+          </div>
         </div>
       </main>
     </StyledCafeCard>
--- client/src/styledComponents/StyledCafeCard.jsx
+++ client/src/styledComponents/StyledCafeCard.jsx
@@ -7,12 +7,91 @@ export const StyledCafeCard = styled.div`
   width: 90%;
   max-width: 600px;
   background-color: ${lighten(0.28, colors.gray)};
+  border-radius: var(--border-radius);
+  padding: var(--space-s);
 
   header {
-    display: flex;
+    display: grid;
+    grid-template-rows: 116px auto;
+
+    @media ${device.tabletPortrait} {
+      grid-template-columns: 116px auto;
+    }
+
+    > figure {
+      width: 100px;
+      height: 100px;
+      border-radius: 100%;
+      overflow: hidden;
+      margin: var(--space-s) auto;
+      @media ${device.tabletPortrait} {
+        margin: var(--space-xs);
+      }
+      > img {
+        width: 100px;
+        height: 100px;
+        object-fit: cover;
+      }
+    }
     > div {
+      margin: var(--space-xs);
+      width: auto;
       display: flex;
       flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      > h3 {
+        margin: var(--space-xs);
+      }
+      > span {
+        padding: 0 var(--space-xs);
+      }
+      @media ${device.tabletPortrait} {
+        text-align: left;
+        > h3 {
+          margin: 0;
+          padding: 0 var(--space-xs);
+        }
+      }
     }
   }
+
+  main {
+    margin: var(--space-xs);
+    
+
+    > h3 {
+      margin: var(--space-xs) 0;
+    }
+
+    > .listing {
+        display: flex;
+        border: 1px solid red;
+    
+    > .cake-info {
+      padding-left: var(--space-xs);
+      display: flex;
+      flex-direction: row;
+
+      border: 1px solid blue;
+
+      /* @media ${device.tabletPortrait} {
+        align-items: center;
+        margin: 0;
+      } */
+
+      > .tag-container {
+        display: flex;
+        margin: var(--space-xs) 0;
+        @media ${device.tabletPortrait} {
+          margin: 0 var(--space-xs);
+        }
+      }
+      > .progressBar {
+        border: 1px solid green;
+        width: 50px;
+    }
+    }
+  }
+    
 `;
