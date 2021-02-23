c805d80e13c8620ab1357985bfaf26ff952202c1
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-23 15:21:54 +0100

    debugging validators

--- controllers/validControllers.js
+++ controllers/validControllers.js
@@ -19,11 +19,11 @@ validateData.register = (req, res, next) => {
   console.log(req.body);
 
   req.check("firstName", "firstName").custom((value) => {
-    return value.match(/^[A-Za-zäöüß]+$/);
+    return customRules.germanLetters(value);
   });
 
   req.check("lastName", "lastName").custom((value) => {
-    return value.match(/^[A-Za-zäöüß]+$/);
+    return customRules.germanLetters(value);
   });
 
   if (req.body.userType === "cafe") {
@@ -32,7 +32,7 @@ validateData.register = (req, res, next) => {
     req.check("cafeStreetNr", "number").isAlphanumeric();
     req.check("cafeZip", "zip").isNumeric();
     req.check("city", "city").custom((value) => {
-      return value.match(/^[A-Za-zäöüß]+$/);
+      return customRules.germanLetters(value);
     });
   }
 
@@ -69,13 +69,13 @@ validateData.updateProfile = (req, res, next) => {
     req.check("cafeStreetNr", "number").isAlphanumeric();
     req.check("cafeZip", "zip").isNumeric();
     req.check("firstName", "firstName").custom((value) => {
-      return value.match(/^[A-Za-zäöüß]+$/);
+      return customRules.germanLetters(value);
     });
     req.check("lastName", "lastName").custom((value) => {
-      return value.match(/^[A-Za-zäöüß]+$/);
+      return customRules.germanLetters(value);
     });
     req.check("city", "city").custom((value) => {
-      return value.match(/^[A-Za-zäöüß]+$/);
+      return customRules.germanLetters(value);
     });
   }
 
@@ -94,26 +94,18 @@ validateData.updateProfile = (req, res, next) => {
 
 validateData.newListing = (req, res, next) => {
   console.log(req.body);
-  req.check("listingName", "listingName").isLength({ min: 1 });
+  req.check("listingName", "listingName").isLength({ min: 1, max: 50 });
   req
     .check("totalPieces", "totalPieces")
     .isNumeric()
     .custom((value) => {
-      if (value > 0) {
-        return true;
-      } else {
-        return false;
-      }
+      return customRules.minimalNumberValue(value, 0);
     });
   req
     .check("piecePrice", "piecePrice")
     .isNumeric()
     .custom((value) => {
-      if (value > 0) {
-        return true;
-      } else {
-        return false;
-      }
+      return customRules.minimalNumberValue(value, 0);
     });
   req.check("pickUpDate", "pickUpDate").isAfter();
 
@@ -123,6 +115,23 @@ validateData.newListing = (req, res, next) => {
     next();
   } else {
     res.send({ msg: errors });
+  }
+};
+
+const customRules = {};
+
+customRules.germanLetters = (value) => {
+  if (!value) {
+    return false;
+  }
+  return value.match(/^[A-Za-zäöüß]+$/);
+};
+
+customRules.minimalNumberValue = (value, minimum) => {
+  if (value > minimum) {
+    return true;
+  } else {
+    return false;
   }
 };
 
