3290013304373e431197655ebaaa89aa3de091de
OlaFro <olka.be@gmail.com>
2021-02-25 15:10:26 +0100

    conditions to display different steps

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import OrderSummary from "./OrderSummary";
 import OrderPayment from "./OrderPayment";
-import OrderConfirmation from "./OrderSummary";
+import OrderConfirmation from "./OrderConfirmation";
 
 import { StyledOrderContainer } from "../styledComponents/StyledOrder";
 
 export default function Order() {
+  const [status, setStatus] = useState({
+    summary: true,
+    payment: false,
+    confirmation: false,
+  });
+
   return (
     <StyledOrderContainer>
-      <OrderSummary />
-      {/* <OrderPayment />
-      <OrderConfirmation /> */}
+      {status.summary ? <OrderSummary /> : null}
+      {status.payment ? <OrderPayment /> : null}
+      {status.confirmation ? <OrderConfirmation /> : null}
     </StyledOrderContainer>
   );
 }
