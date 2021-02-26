eae26730c2481391cfd00d115b9d3fd09ce1246c
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-26 18:01:01 +0100

    test

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -8,6 +8,19 @@ import { StyledOrderContainer } from "../styledComponents/StyledOrder";
 export default function Order() {
   const [step, setStep] = useState("summary");
 
+  console.log(window.location.href.split("/order")[0]);
+
+  console.log(JSON.parse(sessionStorage.getItem("orderInfo")));
+
+  const urlTest =
+    window.location.href.split("/order")[0] +
+    "/cafe:" +
+    JSON.parse(sessionStorage.getItem("orderInfo")).cafeId +
+    "#" +
+    JSON.parse(sessionStorage.getItem("orderInfo")).listingIdentifier;
+
+  console.log(urlTest);
+
   return (
     <StyledOrderContainer>
       {step === "summary" ? <OrderSummary /> : null}
