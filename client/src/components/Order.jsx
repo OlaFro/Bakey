b10d513e146bb8afda12cd452e9d7e464f3c6e42
OlaFro <olka.be@gmail.com>
2021-02-27 16:22:39 +0100

    warning in the order component added

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import OrderSummary from "./OrderSummary";
 import OrderPayment from "./OrderPayment";
 import OrderConfirmation from "./OrderConfirmation";
+import Warning from "./Warning";
 
 import { StyledOrderContainer } from "../styledComponents/StyledOrder";
 
 export default function Order() {
   const [step, setStep] = useState("summary");
+
+  useEffect(() => {
+    if (sessionStorage.orderInfo === undefined) {
+      setStep("warning");
+    }
+  }, []);
 
   const changeStep = (status) => {
     setStep(status);
@@ -14,6 +21,7 @@ export default function Order() {
 
   return (
     <StyledOrderContainer>
+      {step === "warning" ? <Warning msg="there is no order yet" /> : null}
       {step === "summary" ? <OrderSummary change={changeStep} /> : null}
       {step === "payment" ? <OrderPayment change={changeStep} /> : null}
       {step === "confirmation" ? (
