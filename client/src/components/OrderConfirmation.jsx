e7f2ca5e721de06099eaee20efb35bfeb9718af2
OlaFro <olka.be@gmail.com>
2021-02-27 15:05:00 +0100

    copy url functinality and dynamic icons

--- client/src/components/OrderConfirmation.jsx
+++ client/src/components/OrderConfirmation.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { SpinnerRoundFilled } from "spinners-react";
 import colors from "../styledComponents/colors";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import {
   StyledConfirmationContainer,
   StyledLoader,
   StyledCongrats,
+  StyledLinkContainer,
+  StyledLink,
+  StyledCopied,
 } from "../styledComponents/StyledConfirmation";
 
-export default function OrderConfirmation() {
+export default function OrderConfirmation(props) {
   const [loader, setLoader] = useState(true);
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => {
+    setCopied(true);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -26,7 +35,7 @@ export default function OrderConfirmation() {
         <SpinnerRoundFilled
           size={90}
           thickness={180}
-          speed={101}
+          speed={131}
           color={colors.accent2}
           secondaryColor={colors.accent1}
         />
@@ -35,13 +44,25 @@ export default function OrderConfirmation() {
         <p>Your payment was successful.</p>
         <h1>Congrats!</h1>
         <p>
-          <strong>You have just supported Your local cafe ❤️</strong>
+          <strong>You just have supported Your local cafe ❤️</strong>
         </p>
         <p>
           And if the campaign will be successful too, you will soon eat your
-          cake! To increase the chances for meeting the goal of the campaign,{" "}
+          cake! To increase the chance for meeting the goal of the campaign,{" "}
           <strong>invite your friends!</strong>
         </p>
+        <div onClick={handleClick}>
+          <CopyToClipboard
+            text={props.urlListing ? `${props.urlListing}` : "TEST COPIED URL"}
+          >
+            <StyledLinkContainer>
+              {props.urlListing ? props.urlListing : "TEST COPIED URL"}
+            </StyledLinkContainer>
+          </CopyToClipboard>
+
+          <StyledLink display={copied ? "none" : "inline"} />
+          <StyledCopied display={copied ? "inline" : "none"} />
+        </div>
       </StyledCongrats>
     </StyledConfirmationContainer>
   );
--- client/src/styledComponents/StyledConfirmation.jsx
+++ client/src/styledComponents/StyledConfirmation.jsx
@@ -3,6 +3,8 @@ import { lighten } from "polished";
 import colors from "./colors";
 import device from "./device";
 
+import { CheckDouble, Link } from "styled-icons/boxicons-regular";
+
 export const StyledConfirmationContainer = styled.div``;
 
 export const StyledLoader = styled.div`
@@ -23,4 +25,33 @@ export const StyledCongrats = styled.main`
   p {
     text-align: center;
   }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+`;
+
+export const StyledLinkContainer = styled.div`
+  cursor: pointer;
+  padding: var(--space-s);
+  background-color: ${lighten(0.2, colors.gray)};
+  font-family: monospace;
+`;
+
+export const StyledLink = styled(Link)`
+  width: 2rem;
+  height: 2rem;
+  margin-left: var(--space-xs);
+  cursor: pointer;
+  display: ${(props) => props.display};
+`;
+
+export const StyledCopied = styled(CheckDouble)`
+  width: 2.4rem;
+  height: 2.4rem;
+  margin-left: var(--space-xs);
+  cursor: pointer;
+  display: ${(props) => props.display};
 `;
