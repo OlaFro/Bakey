154757f59beeda4cf8d8e2c7c584004cb777a069
OlaFro <olka.be@gmail.com>
2021-02-27 13:47:28 +0100

    added confirmation text

--- client/src/components/OrderConfirmation.jsx
+++ client/src/components/OrderConfirmation.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { SpinnerRoundFilled } from "spinners-react";
 import colors from "../styledComponents/colors";
 
-import { StyledLoader } from "../styledComponents/StyledConfirmation";
+import {
+  StyledConfirmationContainer,
+  StyledLoader,
+  StyledCongrats,
+} from "../styledComponents/StyledConfirmation";
 
 export default function OrderConfirmation() {
   const [loader, setLoader] = useState(true);
@@ -15,17 +19,30 @@ export default function OrderConfirmation() {
   }, []);
 
   return (
-    <StyledLoader display={loader ? "flex" : "none"}>
-      <span>One moment please</span>
-      {/* Spinner configuration: https://adexin.github.io/spinners/ */}
-      <SpinnerRoundFilled
-        size={90}
-        thickness={180}
-        speed={101}
-        color={colors.accent2}
-        secondaryColor={colors.accent1}
-        // enabled={loader}
-      />
-    </StyledLoader>
+    <StyledConfirmationContainer>
+      <StyledLoader display={loader ? "flex" : "none"}>
+        <span>One moment please</span>
+        {/* Spinner configuration: https://adexin.github.io/spinners/ */}
+        <SpinnerRoundFilled
+          size={90}
+          thickness={180}
+          speed={101}
+          color={colors.accent2}
+          secondaryColor={colors.accent1}
+        />
+      </StyledLoader>
+      <StyledCongrats display={loader ? "none" : "flex"}>
+        <p>Your payment was successful.</p>
+        <h1>Congrats!</h1>
+        <p>
+          <strong>You have just supported Your local cafe ❤️</strong>
+        </p>
+        <p>
+          And if the campaign will be successful too, you will soon eat your
+          cake! To increase the chances for meeting the goal of the campaign,{" "}
+          <strong>invite your friends!</strong>
+        </p>
+      </StyledCongrats>
+    </StyledConfirmationContainer>
   );
 }
--- client/src/styledComponents/StyledConfirmation.jsx
+++ client/src/styledComponents/StyledConfirmation.jsx
@@ -3,11 +3,24 @@ import { lighten } from "polished";
 import colors from "./colors";
 import device from "./device";
 
+export const StyledConfirmationContainer = styled.div``;
+
 export const StyledLoader = styled.div`
   display: ${(props) => props.display};
   flex-direction: column;
   align-items: center;
   span {
     margin: var(--space-m) 0;
+  }
+`;
+
+export const StyledCongrats = styled.main`
+  width: 80%;
+  margin: auto;
+  display: ${(props) => props.display};
+  flex-direction: column;
+  align-items: center;
+  p {
+    text-align: center;
   }
 `;
