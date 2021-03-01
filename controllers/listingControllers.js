4d3b749804f49db4c57e90feadcb2d72a3a1b95b
Alice-Rez <reznickova.alice@seznam.cz>
2021-03-01 15:21:23 +0100

    cleaning code

--- controllers/listingControllers.js
+++ controllers/listingControllers.js
@@ -4,10 +4,6 @@ const listingAction = {};
 listingAction.inactivate = (req, res, next) => {
   const now = new Date();
   now.setHours(24, 0, 0, 0);
-  //   const string = `${now.getFullYear()}-0${
-  //     now.getMonth() + 1
-  //   }-0${now.getDate()}T24:00`;
-  //   const today = new Date(string.toString());
   req.date = now;
   next();
 };
