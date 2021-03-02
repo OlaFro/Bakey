6f61664d6acd8ad54cb4d8da867ea9a68e4d0eff
Alice-Rez <reznickova.alice@seznam.cz>
2021-03-02 09:37:24 +0100

    adding handling get requests

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ app.use("/users", usersRouter);
 app.use("/listings", listingsRouter);
 app.use("/cafes", cafesRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 // // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
 //   next(createError(404));
