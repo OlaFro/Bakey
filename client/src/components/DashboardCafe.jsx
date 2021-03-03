3351c4ff5687957f65f0212ee023443c3adbfafa
Alice-Rez <reznickova.alice@seznam.cz>
2021-03-03 11:05:18 +0100

    request for data & handling response

--- client/src/components/DashboardCafe.jsx
+++ client/src/components/DashboardCafe.jsx
@@ -1,16 +1,42 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { bakeyContext } from "../Context";
 import { Link } from "react-router-dom";
+import Axios from "axios";
+import Warning from "./Warning";
 import {
   StyledPlusLink,
   StyledPlusIcon,
   StyledPlusLinkInfo,
 } from "../styledComponents/StyledPlusLink";
-import StyledCentered from "../styledComponents/StyledCentered";
 import StyledCafeDashboard from "../styledComponents/StyledCafeDashboard";
 
 export default function DashboardUser() {
   const { userName } = useContext(bakeyContext);
+
+  const [listings, setListings] = useState([]);
+
+  const [showWarning, setShowWarning] = useState(false);
+
+  const [emptyListings, setEmptyListings] = useState(false);
+
+  useEffect(() => {
+    Axios({
+      method: "GET",
+      url: `listings/cafe`,
+    })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.length) {
+          setListings(res.data);
+        } else {
+          setEmptyListings(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setShowWarning(true);
+      });
+  }, []);
   return (
     <StyledCafeDashboard>
       <header>
@@ -23,6 +49,7 @@ export default function DashboardUser() {
           </Link>
           <StyledPlusLinkInfo>create a new offer</StyledPlusLinkInfo>
         </StyledPlusLink>
+        {showWarning ? <Warning msg="the service is out of order" /> : null}
         <section>
           <h3>Your current offers:</h3>
         </section>
