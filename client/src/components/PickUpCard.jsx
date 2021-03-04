ae0fed6b51c4db9f3b0d7b2576f2afc28f206298
Alice-Rez <reznickova.alice@seznam.cz>
2021-03-04 18:27:23 +0100

    trying to make table work

--- client/src/components/PickUpCard.jsx
+++ client/src/components/PickUpCard.jsx
@@ -1,13 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { bakeyContext } from "../Context";
 import { StyledButton } from "../styledComponents/StyledButton";
 import {
   StyledPickUpCard,
   StyledMailIcon,
+  StyledBtnContainer,
 } from "../styledComponents/StyledPickUpCard";
 
 export default function PickUpCard(props) {
   const { userName, cafeName } = useContext(bakeyContext);
+
+  const [mailSent, setMailSent] = useState([]);
+
+  const [mailSuccess, setMailSuccess] = useState(false);
 
   const getDate = () => {
     return props.pickUpDate.split("T")[0].split("-").reverse().join(".");
@@ -24,51 +29,95 @@ export default function PickUpCard(props) {
     return hours + ":" + minutes;
   };
 
+  const mailSentCheck = (index) => {
+    setMailSent((prevArray) => {
+      return [...prevArray, index];
+    });
+  };
+
+  const sentAllMails = () => {
+    props.buyers.map((buyer, index) => {
+      if (!mailSent.includes(index)) {
+        if (index === props.buyers.length - 1) {
+          setMailSuccess(true);
+        }
+        return setMailSent((prevArray) => {
+          return [...prevArray, index];
+        });
+      }
+      return null;
+    });
+  };
   return (
     <StyledPickUpCard>
       <p>{getDate()}</p>
       <h3>
         {props.title} #{props.id}
       </h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Customer Email</th>
-            <th>Qty</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.buyers.length
-            ? props.buyers.map((buyer, index) => {
-                const scheduledTime = 5 * index;
-                return (
-                  <tr>
-                    <td>{getTime(scheduledTime)}</td>
-                    <td>{buyer.email}</td>
-                    <td>{props.boughtPieces[index]}</td>
-                    <td>
-                      <a
-                        href={`mailto:${
-                          buyer.email
-                        }&subject=Cake%20order%20through%20Bakey%20-%20pick-up%20notification&body=Dear%20Customer,%0D%0Athank%20you%20very%20much%20for%20your%20support%20of%20our%20cafe.%0D%0A%20Your%20order%20of%20the%20${
-                          props.title
-                        }%20will%20be%20prepared%20for%20pick%20up%20at%20${getDate()}%20in%20${getTime(
-                          scheduledTime
-                        )}.%0D%0ABest%20regards,%0D%0A${userName}%20from%20${cafeName}`}
-                      >
-                        <StyledMailIcon />
-                      </a>
-                    </td>
-                  </tr>
-                );
-              })
-            : null}
-        </tbody>
-      </table>
-      <StyledButton buy>Send Emails</StyledButton>
-      <StyledButton buy>Delivered</StyledButton>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Customer Email</th>
+              <th>Qty</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.buyers.length
+              ? props.buyers.map((buyer, index) => {
+                  const scheduledTime = 5 * index;
+                  return (
+                    <tr>
+                      <td>{getTime(scheduledTime)}</td>
+                      <td>{buyer.email}</td>
+                      <td>{props.boughtPieces[index]}</td>
+                      <td>
+                        <a
+                          href={`mailto:${
+                            buyer.email
+                          }&subject=Cake%20order%20through%20Bakey%20-%20pick-up%20notification&body=Dear%20Customer,%0D%0Athank%20you%20very%20much%20for%20your%20support%20of%20our%20cafe.%0D%0A%20Your%20order%20of%20the%20${
+                            props.title
+                          }%20will%20be%20prepared%20for%20pick%20up%20at%20${getDate()}%20in%20${getTime(
+                            scheduledTime
+                          )}.%0D%0ABest%20regards,%0D%0A${userName}%20from%20${cafeName}`}
+                          className={mailSent.includes(index) ? "sent" : null}
+                          onClick={() => {
+                            mailSentCheck(index);
+                          }}
+                        >
+                          <StyledMailIcon />
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
+      </section>
+      <StyledBtnContainer>
+        <StyledButton
+          buy
+          title="Send email to all customers at once"
+          onClick={sentAllMails}
+        >
+          Send Emails
+        </StyledButton>
+        <StyledButton
+          buy
+          title="Move delivered offer to the archive. You can anytime reactivate it."
+        >
+          Finished
+        </StyledButton>
+      </StyledBtnContainer>
+      {mailSuccess ? (
+        <p>
+          Notification e-mails to your customers with scheduled pick up time
+          were successfuly sent.
+        </p>
+      ) : null}
     </StyledPickUpCard>
   );
 }
--- client/src/styledComponents/StyledPickUpCard.jsx
+++ client/src/styledComponents/StyledPickUpCard.jsx
@@ -1,16 +1,66 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { lighten, darken } from "polished";
 import colors from "./colors";
 import device from "./device";
 import { Mail } from "@styled-icons/foundation/Mail";
 
 export const StyledPickUpCard = styled.article`
+  width: 90%;
+  max-width: 1000px;
   background-color: ${lighten(0.28, colors.gray)};
   border-radius: var(--border-radius);
   padding: var(--space-s);
-  margin-bottom: var(--space-m);
+  margin: 0 auto var(--space-m);
+
+  section {
+    width: 400px;
+    overflow-x: scroll;
+
+    @media ${device.tabletPortrait} {
+      overflow-x: hidden;
+    }
+  }
+
+  table {
+    /* border-collapse: separate;
+    border-spacing: 10px;
+
+    tr,
+    td,
+    th {
+      text-align: start;
+    }
+
+    @media ${device.tabletPortrait} {
+      margin: 0 auto var(--space-m);
+      td:nth-of-type(3) {
+        padding-left: var(--space-xs);
+      }
+    } */
+  }
+
+  a {
+    color: ${colors.accent2};
+  }
+
+  .sent {
+    color: ${colors.gray};
+  }
 `;
 
 export const StyledMailIcon = styled(Mail)`
   width: 25px;
+`;
+
+export const StyledBtnContainer = styled.section`
+  margin: auto;
+  width: 90%;
+  max-width: 350px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  button {
+    margin-bottom: var(--space-m);
+  }
 `;
