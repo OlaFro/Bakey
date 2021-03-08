d210695408d14402457450c65dc24974fda22bbc
WILLOID <socialguille@gmail.com>
2021-03-08 16:38:36 +0100

    changed to new google maps api

--- client/src/components/MapCardView.jsx
+++ client/src/components/MapCardView.jsx
@@ -1,39 +1,27 @@
 import React from "react";
-
-
-import {
-  GoogleMap,
-  withScriptjs,
-  withGoogleMap,
-  Marker,
-} from "react-google-maps";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import StyledMap from "../styledComponents/StyledMap";
 
-
 export default function MapCardView(props) {
-
   const location = props.cafeLocation;
-  console.log("this is the location data: ", location)
 
-  function CafeMap() {
-   return <GoogleMap
-      defaultZoom={14}
-      defaultCenter={{ lat: location.lat, lng: location.lng }}
-    >
-      <Marker position={{ lat: location.lat, lng: location.lng}} />
-    </GoogleMap>;
-  }
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+  };
 
-  const WrappedCafeMap = withScriptjs(withGoogleMap(CafeMap));
+  const center = {
+    lat: location.lat,
+    lng: location.lng,
+  };
 
   return (
     <StyledMap>
-      <WrappedCafeMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
-        loadingElement={<div style={{width: `100%`, height: `100%` }} />}
-        containerElement={<div style={{width: `100%`, height: `100%` }} />}
-        mapElement={<div style={{width: `100%`, height: `100%` }} />}
-      />
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+          <Marker position={{ lat: location.lat, lng: location.lng }} />
+        </GoogleMap>
+      </LoadScript>
     </StyledMap>
   );
 }
