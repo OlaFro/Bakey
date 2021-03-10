f3e75d7b57a7551adb8c73b8aeaa0542cefc9aac
OlaFro <olka.be@gmail.com>
2021-03-10 19:59:53 +0100

    pictures added

--- client/src/styledComponents/StyledAboutUs.jsx
+++ client/src/styledComponents/StyledAboutUs.jsx
@@ -4,3 +4,54 @@ import colors from "./colors";
 import device from "./device";
 
 export const StyledAbout = styled.section``;
+
+export const StyledFaces = styled.header`
+  border: 1px solid red;
+  margin: auto;
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+
+  > div > figure {
+    margin: 0;
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    overflow: hidden;
+    > img {
+      width: 100%;
+      object-fit: contain;
+    }
+  }
+`;
+
+export const StyledCard = styled.main`
+  background-color: ${lighten(0.28, colors.gray)};
+  padding: var(--space-m);
+  width: 90%;
+  max-width: 800px;
+  margin: auto;
+  border-radius: var(--border-radius);
+
+  > h2 {
+    text-align: center;
+    margin-bottom: var(--space-l);
+  }
+
+  > h3 {
+    margin-bottom: calc(-1 * var(--space-xs));
+  }
+  > p {
+    margin-bottom: var(--space-m);
+    line-height: 25px;
+  }
+
+  a {
+    color: #4a4a4a;
+    text-decoration: none;
+    border-bottom: 3px solid #ed8db2;
+    :hover {
+      border-bottom: 3px solid transparent;
+    }
+  }
+`;
