e287ea3f31755c02d51551ac99b740021e0a5804
OlaFro <olka.be@gmail.com>
2021-03-11 20:40:38 +0100

    refactoring logo position to flex

--- client/src/styledComponents/StyledLogo.jsx
+++ client/src/styledComponents/StyledLogo.jsx
@@ -3,6 +3,51 @@ import colors from "../styledComponents/colors";
 import device from "./device";
 import { lighten, darken } from "polished";
 
+// export const StyledLogo = styled.div`
+//   margin: auto;
+//   position: relative;
+//   width: 120px;
+//   height: 120px;
+//   border-radius: 100%;
+//   background-color: white;
+//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+//   > img {
+//     width: 90px;
+//     position: absolute;
+//     top: 45%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+//   }
+//   > span {
+//     text-align: center;
+//     color: ${colors.accent2};
+//     font-family: var(--logo);
+//     font-weight: 700;
+//     position: absolute;
+//     top: 72%;
+//     left: 50%;
+//     transform: translateX(-50%);
+//   }
+
+//   ${(props) =>
+//     props.nav &&
+//     css`
+//       box-shadow: none;
+//       height: 40px;
+//       width: 40px;
+
+//       border-radius: 0;
+
+//       > img {
+//         width: inherit;
+//       }
+//       > span {
+//         padding: 3px 0;
+//         display: none;
+//       }
+//     `}
+// `;
+
 export const StyledLogo = styled.div`
   margin: auto;
   position: relative;
@@ -10,40 +55,20 @@ export const StyledLogo = styled.div`
   height: 120px;
   border-radius: 100%;
   background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
   > img {
     width: 90px;
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    margin-top: 4px;
   }
   > span {
     text-align: center;
     color: ${colors.accent2};
     font-family: var(--logo);
     font-weight: 700;
-    position: absolute;
-    top: 72%;
-    left: 50%;
-    transform: translateX(-50%);
+    margin-top: -13px;
   }
-
-  ${(props) =>
-    props.nav &&
-    css`
-      box-shadow: none;
-      height: 40px;
-      width: 40px;
-
-      border-radius: 0;
-
-      > img {
-        width: inherit;
-      }
-      > span {
-        padding: 3px 0;
-        display: none;
-      }
-    `}
 `;
