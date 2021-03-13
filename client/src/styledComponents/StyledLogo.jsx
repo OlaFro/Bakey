90eac560266b3f5bb2f366b713bb6a9da43aa28b
OlaFro <olka.be@gmail.com>
2021-03-13 16:19:06 +0100

    changed border on cafe card to box-shadow

--- client/src/styledComponents/StyledLogo.jsx
+++ client/src/styledComponents/StyledLogo.jsx
@@ -1,7 +1,6 @@
 import styled, { css } from "styled-components";
 import colors from "../styledComponents/colors";
 import device from "./device";
-import { lighten, darken } from "polished";
 
 export const StyledLogo = styled.div`
   margin: auto;
