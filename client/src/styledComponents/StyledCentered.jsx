d4c2630887cee058793738372de4c925e9aef485
WILLOID <socialguille@gmail.com>
2021-03-17 16:24:40 +0100

    changed styled centered

--- client/src/styledComponents/StyledCentered.jsx
+++ client/src/styledComponents/StyledCentered.jsx
@@ -1,14 +1,21 @@
 import styled, { css } from "styled-components";
+import size from "./device";
 
 const StyledCentered = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
   ${(props) =>
     props.settings &&
     css`
       text-align: center;
     `}
+    @media (max-width: 600px) {
+      padding-left: var(--space-s);
+      padding-right: var(--space-s);
+    }
+  
 `;
 
 export default StyledCentered;
